e28219b543b523a7da2d3fef75e49a0fbd05cc9b
Yevheniy Bilodid <yevheniy.bilodid@portmone.me>
2019-06-18 16:48:28 +0300

    sdk version 1.0.0

---
+++ PortmoneSDK.framework/Headers/PortmoneSDK.h
@@ -0,0 +1,32 @@
+//
+//  PortmoneSDK.h
+//  PortmoneSDK
+//
+//  Created on 2/01/19.
+//  Copyright © 2019 Portmone. All rights reserved.
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PortmoneSDK.
+FOUNDATION_EXPORT double PortmoneSDKVersionNumber;
+
+//! Project version string for PortmoneSDK.
+FOUNDATION_EXPORT const unsigned char PortmoneSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PortmoneSDK/PublicHeader.h>
+
+#import <PortmoneSDK/MfsTextField.h>
+#import <PortmoneSDK/MfsCard.h>
+#import <PortmoneSDK/MfsWebView.h>
+#import <PortmoneSDK/MfsResponse.h>
+#import <PortmoneSDK/MfsIOSLibrary.h>
